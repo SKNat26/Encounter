d04be1a7d97c35a28bb66205c94788e2794474d9
Srabon Nath <sknath373@gmail.com>
2022-10-20 01:49:43 -0400

    finished website

--- main.js
+++ main.js
@@ -1,81 +1,97 @@
 // FRONT PAGE MAIN JS
-// variables
-const usernameInput = document.getElementById("username-input")
-const textInput = document.getElementById("text-input");
-const submitButton = document.getElementById("submit-button");
-
-// clicking the submit button
-submitButton.onclick = function(event) {
-    event.preventDefault();
-    if(usernameInput.value == "" || textInput.value == ""){
-        alert("Fill out all fields");
-    }
-    else{
-        const entry = { // database records entry
-            username: usernameInput.value,
-            message: textInput.value
-        };
-    
-        messageDatabase.push(entry);
-        textInput.value = '';
-    }
-}
 
 const chatBox = document.getElementById("chatDiv");
-
-messageDatabase.on("child_added", addTextMessage);
-
-function addTextMessage(rowData) {
-    const messageObject = rowData.val();
-
-    let messageDiv = makeSingleMessageHTML(messageObject.username, messageObject.message);
-    
-    chatBox.appendChild(messageDiv);
-}
-
-function makeSingleMessageHTML(usernameTxt, messageTxt) {
-    let message = document.createElement("p");
-    message.innerHTML = `${usernameTxt}: ${messageTxt}`;
-
-    return message;
-}
 
 submissionDatabase.on("child_added", addReport);
 
 function addReport(rowData) {
     const reportObject = rowData.val();
 
-    let reportDiv = makeReportHTML(reportObject.image, reportObject.location, reportObject.name, reportObject.extra);
+    let reportDiv = makeReportHTML(reportObject.image, reportObject.location, reportObject.date, reportObject.name, reportObject.extra, reportObject.moreHeading, reportObject.moreImage, reportObject.moreIUCN, reportObject.moreDate, reportObject.moreCoords, reportObject.moreLocation);
 
     chatBox.append(reportDiv);
 }
 
-function makeReportHTML(imageSrc, locationTxt, nameTxt, extraTxt) {
+function makeReportHTML(imageSrc, locationTxt, dateTxt, nameTxt, extraTxt, moreHeadingTxt, moreImageTxt, moreIUCNTxt, moreDateTxt, moreCoordsTxt, moreLocationTxt) {
+    //user
     let report = document.createElement("div");
     let rightReport = document.createElement("div");
     let leftReport = document.createElement("div");
     let animalImage = document.createElement("img");
+    let date = document.createElement("p");
     let location = document.createElement("p");
     let animalName = document.createElement("p");
+    let apiData = document.createElement("div");
+    let apiHeading = document.createElement("p");
+    let apiImage = document.createElement("img");
+    let apiIUCN = document.createElement("p");
+    let apiCoords = document.createElement("p");
+    let apiLocation = document.createElement("p");
+    let apiDate = document.createElement("p");
     let extraInfo = document.createElement("p");
+    let extraInfoDiv = document.createElement("div");
+    let seeMoreBtn = document.createElement("button");
 
     animalImage.classList.add("previewImg");
     location.classList.add("previewTxt");
+    location.classList.add("locationColor");
+    date.classList.add("previewDate")
     animalName.classList.add("previewTxt");
+    animalName.classList.add("nameColor");
     extraInfo.classList.add("previewExtra");
+    extraInfoDiv.classList.add("previewExtraDiv");
+    apiData.classList.add("apiData");
+    apiHeading.classList.add("apiHeading");
+    apiImage.classList.add("apiImage");
+    apiIUCN.classList.add("apiIUCN");
+    apiCoords.classList.add("apiCoords");
+    apiLocation.classList.add("apiLocation");
+    apiDate.classList.add("apiDate");
+    seeMoreBtn.classList.add("seeMore");
     rightReport.classList.add("right-preview");
     leftReport.classList.add("left-preview");
     report.classList.add("report");
 
     animalImage.src = imageSrc;
     location.innerHTML = locationTxt;
+    date.innerHTML = dateTxt;
     animalName.innerHTML = nameTxt;
     extraInfo.innerHTML = extraTxt;
+    apiHeading.innerHTML = moreHeadingTxt;
+    apiImage.src = moreImageTxt;
+    apiIUCN.innerHTML = moreIUCNTxt;
+    apiCoords.innerHTML = moreCoordsTxt;
+    apiLocation.innerHTML = moreLocationTxt;
+    apiDate.innerHTML = moreDateTxt;
 
+    seeMoreBtn.innerHTML = "REVEAL/CLOSE";
+
+    seeMoreBtn.onclick = function(event) {
+        event.preventDefault();
+        if(apiHeading.innerHTML != ""){
+            if(window.getComputedStyle(apiData).display == "none"){
+                apiData.style.display = "flex";
+            }
+            else{
+                apiData.style.display = "none";
+            }
+        }
+    }
+
+    extraInfoDiv.appendChild(date);
+    extraInfoDiv.appendChild(extraInfo);
+    apiData.appendChild(apiHeading);
+    apiData.appendChild(apiImage);
+    apiData.appendChild(apiIUCN);
+    apiData.appendChild(apiDate);
+    apiData.appendChild(apiCoords);
+    apiData.appendChild(apiLocation);
     leftReport.appendChild(animalImage);
     rightReport.appendChild(animalName);
     rightReport.appendChild(location); 
-    rightReport.appendChild(extraInfo); 
+    rightReport.appendChild(extraInfoDiv);
+    rightReport.appendChild(seeMoreBtn);
+    rightReport.appendChild(apiData); 
     
     report.appendChild(leftReport);
     report.appendChild(rightReport);
--- reports.js
+++ reports.js
@@ -6,7 +6,10 @@ let previewFull = false;
 let imageInput = document.getElementById("img-submit");
 let locationInput = document.getElementById("loc-submit");
 let nameInput = document.getElementById("name-submit");
+let dateInput = document.getElementById("date-submit");
+let scientificInput = document.getElementById("scientific-submit");
 let extraInput = document.getElementById("extra-submit");
+let taxonArea = document.querySelector("#taxon p");
 
 let extraString = "";
     
@@ -24,13 +27,17 @@ const options = {
 
 // buttons
 const submitBtn = document.getElementById("data-submit");
+let taxonBtn = document.getElementById("find-taxon");
+const addBtn = document.getElementById("add-more")
 const confirmBtn = document.getElementById("confirm-response");
+
+let extraInfo = document.createElement("p");
 
 submitBtn.onclick = function (event) {
     event.preventDefault();
 
-    if(imageInput.value == "" || locationInput.value == ""){
-        alert("Please provide an image, location, and a name!");
+    if(imageInput.value == "" || locationInput.value == "" || nameInput.value == "" || dateInput.value == ""){
+        alert("Please provide an image, location, date and a name!");
         previewFull = false;
     }
     else {
@@ -41,38 +48,155 @@ submitBtn.onclick = function (event) {
         let animalImage = document.createElement("img");
         let location = document.createElement("p");
         let animalName = document.createElement("p");
-        let extraInfo = document.createElement("p");
+        let date = document.createElement("p");
+        let extraInfoDiv = document.createElement("div");
 
         animalImage.classList.add("previewImg");
         location.classList.add("previewTxt");
+        location.classList.add("locationColor");
+        date.classList.add("previewDate")
         animalName.classList.add("previewTxt");
+        animalName.classList.add("nameColor");
         extraInfo.classList.add("previewExtra");
+        extraInfoDiv.classList.add("previewExtraDiv");
 
         animalImage.src = imageInput.value;
         location.innerHTML = locationInput.value;
+        date.innerHTML = dateInput.value.substring(5,7) + "/" + dateInput.value.substring(8,11) + "/" + dateInput.value.substring(0,4);
         animalName.innerHTML = nameInput.value;
         extraInfo.innerHTML = extraInput.value;
 
         previewLeft.appendChild(animalImage);
         previewRight.appendChild(animalName);
         previewRight.appendChild(location);
-        previewRight.appendChild(extraInfo);
-
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': 'be5579b406msh1f8c2d3ffe74664p193917jsn4f5339d2425a',
-                'X-RapidAPI-Host': 'animaliapi3.p.rapidapi.com'
-            }
-        };
+        extraInfoDiv.appendChild(date);
+        extraInfoDiv.appendChild(extraInfo);
+        previewRight.appendChild(extraInfoDiv);
         
-        fetch(`https://animaliapi3.p.rapidapi.com/all/${processName(nameInput.value)}`, options)
-            .then(response => response.json())
-            .then(response => processAnimalInfo(response.animal,extraInfo))
-            .catch(err => console.error(err));
-
+        if(scientificInput.value == ""){
+            const options = {
+                method: 'GET',
+                headers: {
+                    'X-RapidAPI-Key': 'be5579b406msh1f8c2d3ffe74664p193917jsn4f5339d2425a',
+                    'X-RapidAPI-Host': 'animaliapi3.p.rapidapi.com'
+                }
+            };
+            
+            fetch(`https://animaliapi3.p.rapidapi.com/all/${processName(nameInput.value)}`, options)
+                .then(response => response.json())
+                .then(response => processAnimalInfo(response.animal))
+                .catch(err => console.error(err));
+        }
         previewFull = true;
+
     }
+}
+
+taxonBtn.onclick = function(event) {
+    event.preventDefault();
+
+    if(previewFull && scientificInput.value != "") {
+        baseURL = "https://api.gbif.org/v1/";
+
+        //gbif species match
+        fetch(`${baseURL}species/match?name=${scientificInput.value}`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(retrieveTaxon)
+            .catch(function (error) {
+                console.log("Error during fetch:", error);
+            });
+    }
+}
+
+function retrieveTaxon(data) {
+    taxonArea.innerHTML = "Taxon Key:"
+    taxonArea.innerHTML += " ";
+    taxonArea.innerHTML += data.usageKey;
+}
+
+let apiData = document.createElement("div");
+let apiHeading = document.createElement("p");
+let apiImage = document.createElement("img");
+let apiIUCN = document.createElement("p");
+let apiCoords = document.createElement("p");
+let apiLocation = document.createElement("p");
+let apiDate = document.createElement("p");
+
+addBtn.onclick = function(event) {
+    event.preventDefault();
+
+    if(previewFull && scientificInput.value != ""){
+        let seeMoreBtn = document.createElement("button");
+        seeMoreBtn.innerHTML = "REVEAL/CLOSE";
+
+        seeMoreBtn.onclick = function(event) {
+            event.preventDefault();
+
+            if(window.getComputedStyle(apiData).display == "none"){
+                apiData.style.display = "flex";
+            }
+            else{
+                apiData.style.display = "none";
+            }
+        }
+
+        apiData.classList.add("apiData");
+        apiHeading.classList.add("apiHeading");
+        apiImage.classList.add("apiImage");
+        apiIUCN.classList.add("apiIUCN");
+        apiCoords.classList.add("apiCoords");
+        apiLocation.classList.add("apiLocation");
+        apiDate.classList.add("apiDate");
+        seeMoreBtn.classList.add("seeMore");
+
+        apiData.appendChild(apiHeading);
+        apiData.appendChild(apiImage);
+        apiData.appendChild(apiIUCN);
+        apiData.appendChild(apiDate);
+        apiData.appendChild(apiCoords);
+        apiData.appendChild(apiLocation);
+
+        previewRight.appendChild(seeMoreBtn);
+        previewRight.appendChild(apiData);
+
+        baseURL = "https://api.gbif.org/v1/";
+
+        //gbif occurence search
+        fetch(`${baseURL}occurrence/search?taxonkey=${taxonArea.innerHTML.substring(11)}&limit=1`)
+            .then(response => response.json())
+            .then(response => processOccurences(response,apiHeading,apiImage,apiIUCN,apiCoords,apiLocation,apiDate))
+            .catch(function (error) {
+                console.log("Error during fetch:", error);
+            });
+    }        
+}
+
+function processOccurences(data, heading, availableImg, iucnCat, coords, location, date) {
+    const iucn = {
+        "DD" : "Data Deficient",
+        "LC" : "Least Concern",
+        "NT" : "Near Threatened",
+        "VU" : "Vulnerable",
+        "EN" : "Endangered",
+        "CR" : "Critically Endangered",
+        "EW" : "Extinct in the Wild",
+        "EX" : "Extinct",
+        "NE" : "Not Evaluated"
+    };
+
+    let image = "";
+    if(data.results[0].media.length != 0) {
+        image = data.results[0].media[0].identifier;
+    }
+
+    heading.innerHTML = "Related to this Post:";
+    availableImg.src = image;
+    iucnCat.innerHTML = iucn[(data.results[0].iucnRedListCategory)];
+    date.innerHTML = data.results[0].month + "/" + data.results[0].day + "/" + data.results[0].year;
+    coords.innerHTML = data.results[0].decimalLatitude + ", " + data.results[0].decimalLongitude;
+    location.innerHTML = data.results[0].country + ", " + data.results[0].stateProvince;
 }
 
 confirmBtn.onclick = function (event) {
@@ -85,8 +209,12 @@ confirmBtn.onclick = function (event) {
             previewRight.textContent = "";
             imageInput.value = "";
             locationInput.value = "";
+            dateInput.value = "";
             nameInput.value = "";
             extraInput.value = "";
+            scientificInput.value = "";
+            taxonArea.innerHTML = "";
+            apiData.textContent = "";
             previewFull = false;
         }
     }
@@ -99,8 +227,15 @@ function updateDB() {
     const entry = {
         image: imageInput.value,
         location: locationInput.value,
+        date: dateInput.value,
         name: nameInput.value,
-        extra: extraString
+        extra: extraInfo.innerHTML,
+        moreHeading: apiHeading.innerHTML,
+        moreImage: apiImage.src,
+        moreIUCN: apiIUCN.innerHTML,
+        moreDate: apiDate.innerHTML,
+        moreCoords: apiCoords.innerHTML,
+        moreLocation: apiLocation.innerHTML
     }
 
     submissionDatabase.push(entry);
@@ -113,10 +248,9 @@ function processName(name) {
     return newName;
 }
 
-function processAnimalInfo(data,extra) {
+function processAnimalInfo(data) {
     if(data != undefined){
         console.log(data);
-        extra.innerHTML += `<br/><br/>Class Name: ${data.classname}<br/>Family: ${data.family}<br/>Genus: ${data.genus}<br/>Kingdom: ${data.kingdom}<br/>Order: ${data.order}<br/>Phylum: ${data.phylum}<br/>Scientific Name: ${data.scientificname}`;
-        extraString = extra.innerHTML;
+        scientificInput.value = data.scientificname;
     }
 }
